d611b8ae365a9b21d19f29026dc1907806dd1c22
omjamkhute29 <jamkhuteomjamkhute@gmail.com>
2026-06-25 12:47:28 +0530

    my work done

---
+++ app.js
@@ -0,0 +1,7 @@
+// document object model
+alert("my god");
+window.alert("good job");
+const val = prompt("naam bata","rocky bhai");  // default val
+console.log(val);
+
+console.log(document.title);
